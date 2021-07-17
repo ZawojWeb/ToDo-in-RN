b046345e71b34c0cd083dde6313ab3f6319bf793
Zawoj <zawojweb@gmail.com>
2021-07-17 18:41:58 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("App executed");
+  let x;
   return (
     <View style={styles.container}>
-      <Text>Hello Ract World</Text>
+      <Text>Hello Jestem Andrzej</Text>
       <StatusBar style="auto" />
     </View>
   );
